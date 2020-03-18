446468cfda723a8444ef748d434c37829dad4baa
William D Joshua <yourenlaila@hotmail.com>
2020-03-18 15:21:15 +0800

    add nav menu json model

---
+++ mtsToolCaliburn/Models/NavigateMenusRootObject.cs
@@ -0,0 +1,44 @@
+﻿using MahApps.Metro.IconPacks;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtsToolCaliburn.Models
+{
+    public class NavigateMenusRootObject
+    {
+        public NavigateMenusModel menus { get; set; }
+    }
+    public class NavigateMenusModel
+    {
+        public List<NavigateMenuItem> menu { get; set; }
+    }
+
+    [JsonObject("menu")]
+    public class NavigateMenuItem
+    {
+        public string name { get; set; }
+        public string title { get; set; }
+        public PackIconMaterialKind iconType { get; set; }
+        public string url { get; set; }
+        public Boolean subArrow { get; set; }
+
+        public NavigateSubMenuModel submenus { get; set; }
+    }
+
+
+    public class NavigateSubMenuModel
+    {
+        public List<NavigateSubMenuItem> submenu{ get; set; }
+    }
+
+    public class NavigateSubMenuItem
+    {
+        public string name { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+}
--- mtsToolCaliburn/ViewModels/Components/NavigateBarViewModel.cs
+++ mtsToolCaliburn/ViewModels/Components/NavigateBarViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Caliburn.Micro;
+using mtsToolCaliburn.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +12,20 @@ namespace mtsToolCaliburn.ViewModels.Components
 {
     public class NavigateBarViewModel : PropertyChangedBase
     {
+        private string navMenuItemsAddr = string.Format("{0}/Resources/navMenuItems.json", Environment.CurrentDirectory);
+
         public UserBusinessCardViewModel UserBussinessCard { get; }
 
-        public  NavigateItemViewModel NavBarItems { get; }
+        public NavigateItemViewModel NavBarItems { get; }
         public NavigateBarViewModel()
         {
+            NavigateMenusRootObject NavigateMenus = new NavigateMenusRootObject();
+            
+            using (StreamReader reader = new StreamReader(navMenuItemsAddr))
+            {
+                JsonSerializer jsonSerializer = new JsonSerializer();
+                NavigateMenus =(NavigateMenusRootObject) jsonSerializer.Deserialize(reader,typeof(NavigateMenusRootObject));
+            }
             UserBussinessCard = new UserBusinessCardViewModel();
             NavBarItems = new NavigateItemViewModel();
         }
