aa753e31befbaaf694e852b164c831eae90bf5e5
William D Joshua <yourenlaila@hotmail.com>
2020-07-03 16:33:02 +0800

    add restapi bridge router

---
+++ mtsToolCaliburn.Controller/BirdgeRouter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtsToolCaliburn.Controller
+{
+    /// <summary>
+    /// 数据路由
+    /// </summary>
+    public class BirdgeRouter
+    {        
+        /// <summary>
+             /// 桥接抽象层
+             /// </summary>
+        public static BirdgeRouterController BirdgeRouterController { get; private set; }
+
+        public BirdgeRouter(BirdgeRouteAddress birdgeRouteAddress)
+        {
+            switch(birdgeRouteAddress)
+            {
+                case BirdgeRouteAddress._mts_api_client: BirdgeRouterController = new MtsapiRouterController();  break;
+                case BirdgeRouteAddress._k3_api_client: BirdgeRouterController = new K3apiRouterController(); break;
+                case BirdgeRouteAddress._oa_api_client: BirdgeRouterController = new OaapiRouterController(); break;
+                case BirdgeRouteAddress._local_wcf_client: BirdgeRouterController = new WcflocalRouterController(); break;
+                default: BirdgeRouterController = new MtsapiRouterController(); break;
+            }
+    }
+    }
+
+    /// <summary>
+    /// 数据桥接路由类型
+    /// </summary>
+    public enum BirdgeRouteAddress
+    {
+        _mts_api_client, // MTS Web API
+        _k3_api_client,  // K3 Web API
+        _oa_api_client, // OA Web API
+        _local_wcf_client  // localhost WCF
+    }
+}
